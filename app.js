dcf0c49e3f87204b3ac3579ff53e7ba7e62b7dfc
HitomiHuang <black011602@gmail.com>
2022-01-12 22:34:51 +0800

    feat: add create todo function

--- app.js
+++ app.js
@@ -18,12 +18,24 @@ db.once('open', () => {
 
 app.engine('hbs', exphbs.engine({defaultLayout: 'main', extname: '.hbs'}))
 app.set('view engine', 'hbs')
+app.use(express.urlencoded({ extended:true }))
 
 app.get('/', (req, res) => {
   Todo.find()
     .lean()
     .then(todos => res.render('index', { todos }))
     .catch(error => console.error(error))
+})
+
+app.get('/todos/new', (req, res) => {
+  return res.render('new')
+})
+
+app.post('/todos', (req, res) => {
+  const name = req.body.name
+  return Todo.create({ name })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
 })
 
 app.listen(port, () => {
