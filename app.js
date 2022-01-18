3c41c32128c2b186b0cf4829460f85da336baf6e
HitomiHuang <black011602@gmail.com>
2022-01-19 02:49:05 +0800

    refractor: modify mongoose config

--- app.js
+++ app.js
@@ -1,22 +1,11 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
-const Todo = require('./models/todo')
-const mongoose = require('mongoose')
 const app = express()
 const port = 3000
 const methodOverride = require('method-override')
+
 const routes = require('./routes')
-
-mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true }, { useUnifiedTopology: true })
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
+require('./config/mongoose')
 
 app.engine('hbs', exphbs.engine({defaultLayout: 'main', extname: '.hbs'}))
 app.set('view engine', 'hbs')
